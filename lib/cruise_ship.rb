40d68544c661d2b258c9dd4dcf0c4ab5e2dbf354
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-10 04:16:42 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,6 @@ suite_e: "Crumpet the Elf"
 def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
-    p :suite_a
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
